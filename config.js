cdab15f2917a53bb99a3c8a1dd38ae1f2aa1a24b
Aditya13114 <75022510+Aditya13114@users.noreply.github.com>
2021-05-07 18:56:14 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+require('@firebase/firestore');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCEAkJ9nlDUvBkDl4HnZ7C2db33CIIyv9w",
+    authDomain: "c-77-3711e.firebaseapp.com",
+    projectId: "c-77-3711e",
+    storageBucket: "c-77-3711e.appspot.com",
+    messagingSenderId: "263535254340",
+    appId: "1:263535254340:web:4bbbf1434159779497d511"
+  };
+
+  
+
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore(); 
+  
